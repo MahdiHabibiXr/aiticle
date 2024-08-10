db3a0a87c93ee7b6959f2306ef7f2dd5d9418fd5
MahdiHabibiXr <themahdihabibi@gmail.com>
2024-08-10 10:48:31 +0330

    added request

--- app/request.py
+++ app/request.py
@@ -4,7 +4,7 @@ api = 'c57a1aa2-365a-4111-aa93-329b0658332c:122e40e0a920017aa9e4e1e326fd5fb8'
 # Define the URL and headers
 url = "https://fal.run/fal-ai/clarity-upscaler"
 headers = {
-    "Authorization": f"KEY {api}",  # Replace YOUR_FAL_KEY with the actual key
+    "Authorization": f"Key {api}",  # Replace YOUR_FAL_KEY with the actual key
     "Content-Type": "application/json"
 }
 
@@ -13,8 +13,13 @@ data = {
     "image_url": "https://storage.googleapis.com/falserverless/gallery/NOCA_Mick-Thompson.resized.resized.jpg"
 }
 
-# Make the POST request
-response = requests.post(url, headers=headers, json=data)
+# Define the proxies
+proxies = {
+    "http": "fodev.org:8118",
+}
+
+# Make the POST request through the proxy
+response = requests.post(url, headers=headers, json=data, proxies=proxies)
 
 # Print the response
 print(response.status_code)
