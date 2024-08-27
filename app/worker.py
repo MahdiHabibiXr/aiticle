657bad32648eb8237a10f9e70233e5efd861ddd6
MahdiHabibiXr <themahdihabibi@gmail.com>
2024-08-28 00:53:37 +0330

    changed the bot and worker

--- app/worker.py
+++ app/worker.py
@@ -4,8 +4,8 @@ import db
 import models 
 from pyrogram import Client, filters
 
-# bot = Client('mahdi2',api_id=863373,api_hash='c9f8495ddd20615835d3fd073233a3f6' )
-bot = Client('mahdi2')
+bot = Client('mahdi2',api_id=863373,api_hash='c9f8495ddd20615835d3fd073233a3f6' )
+# bot = Client('mahdi2')
 
 def check_and_update_tasks():
     # Get all jobs from the database
@@ -23,7 +23,7 @@ def check_and_update_tasks():
             with bot:
                 bot.send_message(int(user), 'عکس شما با موفقیت ساخته شد، بفرمایید :')
                 bot.send_photo(int(user), result, 'ساخته شده با عکسیفای')
-                
+
             # bot.send_photo(int(user), user_image)
 
             print(f"Job {task[0]} done. Going to change it into the database")
